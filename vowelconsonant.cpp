be3408ee0b7d95111abb84891356c3cb713ec8e9
Adip721 <147860513+Adip721@users.noreply.github.com>
2023-10-15 23:34:37 +0530

    Add files via upload

---
+++ vowelconsonant.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+int main(){
+    char x;
+    std::cout<<"Please Enter your character:";
+    std::cin >> x;
+    switch(x){
+        case 'a':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'e':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'i':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'o':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'u':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'A':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'E':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'I':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'O':
+        std::cout<<"This character is a vowel";
+        break;
+        case 'U':
+        std::cout<<"This character is a vowel";
+        break;
+        default:
+        std::cout<<"This Character is a consonant";
+
+    }
+    return 0;
+}
+    
+    
+
